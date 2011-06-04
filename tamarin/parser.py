ccb6fc1e05ff6d4301685b8b797a9d44004090cf
Stefan Haflidason <stefan@stallic.com>
2011-06-04 18:06:24 +0900

    Added CloudFront log parser, and more characters to the list of acceptable characters for an S3 key

--- tamarin/parser.py
+++ tamarin/parser.py
@@ -103,7 +103,7 @@ class S3LogLineParser(object):
         # IE: SOAP.CreateBucket or REST.PUT.OBJECT
         operation = Word(alphas + "._")
         # S3 key: /photos/2006/08/puppy.jpg
-        key = Word(alphanums + "/-_.?=%&:+<>#~[]")
+        key = Word(alphanums + "/-_.?=%&:+<>#~[]{}+!")
         # One of GET, POST, or PUT
         http_method = Word(alphas)
         # HTTP/1.1
@@ -153,10 +153,6 @@ class S3LogLineParser(object):
             user_agent_or_dash('user_agent') +
             alpha_or_dash('version_id')
         )
-        # HACK: Some closing curly braces have found their way into the logs by virtue of some
-        # bad Django template code (I presume). Removing them for now until a better solution
-        # presents itself.
-        self.line_contents = self.line_contents.replace('}', '')
         return log_line_bnf.parseString(self.line_contents)
 
     def _parse_result_debug_msg(self, parsed):
@@ -172,6 +168,103 @@ class S3LogLineParser(object):
         for field in self.FIELDS:
             retval += "%s: %s\n" % (field, parsed.get(field, ''))
         return retval
+
+class CloudFrontLogLineParser(S3LogLineParser):
+    """
+    Parses individual lines within a CloudFront distribution log. Spits out 
+    CloudFrontLogRecord objects.
+    """
+    FIELDS = (
+        'request_date_string', 'request_time_string', 'edge_location', 'bytes_sent',
+        'remote_ip', 'http_method', 'cs_host', 'cs_uri_stem', 'http_status',
+        'referrer', 'user_agent', 'query_string',
+    )
+
+    def __init__(self, line_contents):
+        """
+        :param str line_contents: The individual line/log entry to parse, in
+            string form.
+        """
+        super(CloudFrontLogLineParser, self).__init__(line_contents)
+    
+    def parse(self):
+        """
+        Parses the class's :attr:`line_contents` attribute using pyparsing.
+        
+        :rtype: pyparsing.ParseResults
+        :returns: A pyparsing ParseResults instance, which can be used much
+            like a dict.
+        """
+        # Some generic parsing patterns.
+        integer = Word(nums)
+        generic_alphanum = Word(alphanums)
+        alpha_or_dash = Word(alphas + "-")
+        num_or_dash = Word(nums + '-')
+        url = Word(alphanums + "/-_.?=%&:+<>#~[]{}+!")
+
+        date = Combine(
+                integer + '-' +
+                integer + '-' +
+                integer
+            )
+        
+        time = Combine(
+                integer + ':' +
+                integer + ':' +
+                integer
+            )
+        
+        edge_location = Word(alphanums)
+        
+        bytes_sent = integer
+        
+        # 72.21.206.5
+        ip_address = delimitedList(integer, ".", combine=True)
+        
+        # One of GET, POST, or PUT
+        http_method = Word(alphas)
+        
+        # CloudFront distribution underlying DNS name
+        cs_host = Word(alphanums + '-_.')
+        
+        # Equivalent to key in the S3 case
+        cs_uri_stem = url
+        
+        http_status = integer
+        
+        referrer = url
+        
+        # "curl/7.15.1"
+        user_agent = Word(alphanums + "/-_.?=%&:();,+$@!^<>~[]'{}#*`")
+                     
+        query_string = Word(alphanums + '?&=-_.')
+        
+        # Referrer can be empty double quotes sometimes.
+        empty_dquotes = Suppress('"') + Suppress('"')
+        # Either a referrer, a dash, or empty double quotes.
+        query_string_or_dash = query_string | "-" | empty_dquotes
+        
+        # User agent field can either be a user agent string or a dash.
+        user_agent_or_dash = user_agent | "-" | empty_dquotes
+
+        # The string value for each field below is what you refer to when
+        # accessing the parsed values.
+        log_line_bnf = (
+            date("request_date_string") + 
+            time("request_time_string") + 
+            edge_location("edge_location") + 
+            bytes_sent("bytes_sent") + 
+            ip_address("remote_ip") + 
+            http_method("http_method") + 
+            cs_host("cs_host") + 
+            cs_uri_stem("cs_uri_stem") + 
+            http_status("http_status") + 
+            referrer("referrer") + 
+            user_agent_or_dash("user_agent") + 
+            query_string_or_dash("query_string")
+        )
+        
+        return log_line_bnf.parseString(self.line_contents)
 
 class S3LogParser(object):
     """
@@ -199,5 +292,28 @@ class S3LogParser(object):
                 continue
             # Delegate to the line parser.
             line_parser = S3LogLineParser(line)
+            # Return the pyparsing.ParseResults for the line.
+            yield line_parser.parse_line()
+
+class CloudFrontLogParser(S3LogParser):
+    def __init__(self, log_contents):
+        super(CloudFrontLogParser, self).__init__(log_contents)
+        
+    def parse_lines(self):
+        """
+        Breaks the file up into lines and delegates the parsing of each
+        line to :class:`S3LogLineParser`.
+        
+        :rtype: pyparsing.ParseResults
+        :returns: A pyparsing ParseResults instance, which can be used much
+            like a dict.
+        """
+        lines = self.log_contents.split('\n')
+        for line in lines:
+            if not line:
+                # Empty line, skip.
+                continue
+            # Delegate to the line parser.
+            line_parser = CloudFrontLogLineParser(line)
             # Return the pyparsing.ParseResults for the line.
             yield line_parser.parse_line()
